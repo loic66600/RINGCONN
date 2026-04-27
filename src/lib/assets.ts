34c2dab02b7497632fc3b1fd3a700ac3fe9206aa
loic <loic.rossignol66@gmail.com>
2026-04-27 20:03:45 +0200

    Improve mobile layout and CTAs

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -1,12 +1,13 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const assets = {
-  ring: "/images/ring%20com%202%20air.png",
-  ring2: "/images/ring2.png",
-  autonomy: "/images/autonomie.png",
-  course: "/images/course.png",
-  personSport: "/images/gen2air_3.webp",
-  personLifestyle: "/images/gen2air_4.webp",
-  testimonialCraft: "/images/gen2_PDP_2.webp",
-  testimonialSleep: "/images/gen2_PDP_PC_1.webp",
-  video:
-    "/images/RingConn%20Gen%202%20Smart%20Ring%20Suivi%20de%20lapn%C3%A9e%20du%20Sommeil%20in%C3%A9dit%2012%20J.mp4",
+  ring: `${BASE_URL}images/ring%20com%202%20air.png`,
+  ring2: `${BASE_URL}images/ring2.png`,
+  autonomy: `${BASE_URL}images/autonomie.png`,
+  course: `${BASE_URL}images/course.png`,
+  personSport: `${BASE_URL}images/gen2air_3.webp`,
+  personLifestyle: `${BASE_URL}images/gen2air_4.webp`,
+  testimonialCraft: `${BASE_URL}images/gen2_PDP_2.webp`,
+  testimonialSleep: `${BASE_URL}images/gen2_PDP_PC_1.webp`,
+  video: `${BASE_URL}images/RingConn%20Gen%202%20Smart%20Ring%20Suivi%20de%20lapn%C3%A9e%20du%20Sommeil%20in%C3%A9dit%2012%20J.mp4`,
 };
